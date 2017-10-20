fef657d1c93c4a4baae82096136ace5be05b62ec
zfarrel1 <32079991+zfarrel1@users.noreply.github.com>
2017-10-19 22:23:18 -0400

    JS lab

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,26 @@
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 + 17;
+var myName = "Zinnia Farrell"
+
+	document.write(num1);
+	document.write(num2);
+	document.write(num3);
+	document.write(myName);
+
+function hello () {
+	  var greeting = 'hello!';
+
+	  greeting = 'hello, world!';
+
+	  alert(greeting);
+	}
+
+	hello ();
+
+	console.log('"hello, world!"')
+
+	document.querySelector('#title').style.color = 'red';
+	document.querySelector('#text-chunk').style.color = 'blue';
+
+
